afaf9c5137332f29ef3f2f4c62fdb9407a2bac6b
dedurak <36812630+dedurak@users.noreply.github.com>
2020-12-04 09:52:21 +0100

    Update backend.js

--- utils/backend.js
+++ utils/backend.js
@@ -6,6 +6,7 @@ export class Backend {
     depTime= "";
     arrTime= "";
     opDays = [];
+    day=0;
     date = new Object();
     flightTime = "";
 
@@ -13,12 +14,120 @@ export class Backend {
     search_arr = "";
     search_depTime = [];
     search_arrTime = [];
-    search_flightTime = [];
+    sFlightTime = [];
     search_flightNumber = [];
-    sOpDays = new Map();
+    search_opDays = new Map();
     sPrices = new Map();
     arrIndex = 0;
 
+    myFlights = [];
+    myName = [];
+    myBDate = [];
+    mySurname = [];
+    myFrom = [];
+    myTo = [];
+    myDepTime = [];
+    myArrTime = [];
+    myFlightDate = [];
+    myPassengerStatus = [];
+    myFlightStatus = [];
+    myPrice = [];
+    myPriceTaxes = [];
+    detailsIndex = 0;
+
+    setMyFlight(param) { this.myFlights.push(param)}
+    getMyFlight(param) { return this.myFlights[param]}
+
+    setMyName(param) { this.myName.push(param)}
+    getMyName(param) { return this.myName[param]}
+
+    setMySurname(param) { this.mySurname.push(param)}
+    getMySurname(param) { return this.mySurname[param]}
+
+    setMyBDate(param) { this.myBDate.push(param)}
+    getMyBdate(param) { return this.myBDate[param]}
+
+    setMyFrom(param) { this.myFrom.push(param)}
+    getMyFrom(param) { return this.myFrom[param]}
+
+    setMyTo(param) { this.myTo.push(param)}
+    getMyTo(param) { return this.myTo[param]}
+
+    setMyDepTime(param) { this.myDepTime.push(param)}
+    getMyDepTime(param) { return this.myDepTime[param]}
+
+    setMyArrTime(param) { this.myArrTime.push(param)}
+    getMyArrTime(param) { return this.myArrTime[param]}
+
+    setMyFlightDate(param) { this.myFlightDate.push(param)}
+    getMyFlightDate(param) { return this.myFlightDate[param]}
+
+    setMyPassengerStatus(param) { this.myPassengerStatus.push(param)}
+    getMyPassengerStatus(param) { return this.myPassengerStatus[param]}
+
+    setMyFlightStatus(param) { this.myFlightStatus.push(param)}
+    getMyFlightStatus(param) { return this.myFlightStatus[param]}
+
+    setMyPrice(param) { this.myPrice.push(param)}
+    getMyPrice(param) { return this.myPrice[param]}
+
+    setMyPriceTaxes(param) { this.myPriceTaxes.push(param)}
+    getMyPriceTaxes(param) { return this.myPriceTaxes[param]}
+
+    setDetailsIndex(param) { this.detailsIndex=param }
+    getDetailsIndex() { return this.detailsIndex }
+
+    cleanMyValues() {
+        this.myFlights = [];
+        this.myName = [];
+        this.mySurname = [];
+        this.myFrom = [];
+        this.myTo = [];
+        this.myDepTime = [];
+        this.myArrTime = [];
+        this.myFlightDate = [];
+        this.myPassengerStatus=[];
+        this.myFlightStatus = [];
+    }
+
+    getMyArrayLength() {
+        var ind = 0;
+        var retArr = [];
+
+        for(; ind<this.myDepTime.length; ) {
+            retArr.push(ind++)
+        }
+
+        return retArr;
+    }
+
+    bookingIndex = 0;
+    bookingCID = "";
+    indexForFlightBooking=0;
+
+    /**
+     * search_dep = "";
+    search_arr = "";
+    search_depTime = [];
+    search_arrTime = [];
+    sFlightTime = [];
+    search_flightNumber = [];
+    search_opDays = new Map();
+    sPrices = new Map();
+    arrIndex = 0;
+     */
+
+    clearSearchParams() {
+        this.search_dep="";
+        this.search_arr="";
+        this.search_depTime = [];
+        this.search_arrTime = [];
+        this.sFlightTime = [];
+        this.search_flightNumber = [];
+        this.search_opDays = new Map();
+        this.sPrices = new Map();
+        this.arrIndex = 0;
+    }
 
     cleanAllValues() {
         this.search_depTime=[];
@@ -26,9 +135,22 @@ export class Backend {
         this.search_dep="";
         this.search_arrTime=[];
         this.search_arr="";
-        this.search_opDays=[];
-        this.search_flightTime=[];
+        this.search_opDays=new Map();
+        this.search_flightTime=new Map();
+        this.sPrices = new Map();
+        this.arrIndex = 0;
     }
+
+
+    setBookingIndex(param) { this.bookingIndex = param}
+    getBookingIndex() { return this.bookingIndex}
+
+    setBookingCID(param) { this.bookingCID = param}
+    getBookingCID() { return this.bookingCID }
+
+
+    setIndexforFlightBooking(param) { this.indexForFlightBooking = param}
+    getIndexforFlightBooking() { return this.indexForFlightBooking}
 
 
     setDate(param) { this.date = param; }
@@ -55,6 +177,9 @@ export class Backend {
     setFlightTime(param) { this.flightTime = param}
     getFlightTime() { return this.flightTime }
 
+    setDay(param) { this.day=param}
+    getDay() { return this.day}
+
 
 
     setSearchDep(param) { this.searchDep = param }
@@ -72,22 +197,16 @@ export class Backend {
     setSearchFlightNumber(param) { this.search_flightNumber.push(param)}
     getSearchFlightNumber(param) { return this.search_flightNumber[param] }
 
-    setSearchFlightTime(param) { this.search_flightTime.push(param)}
-    getSearchFlightTime(para) { return this.search_flightTime[para] }
-
-    setSearchOpDay(param) { this.search_opDays.push(param) }
-    getSearchOpDay(param) { return this.search_opDays[param] }
-
-    setSearchPrice(param) { this.search_prices.push(param)}
-    getSearchPrice(param) { this.search_prices[param]}
+    setSearchFlightTime(param) { this.sFlightTime.push(param) }
+    getSearchFlightTime(para) { return this.sFlightTime[para] }
 
     setSearchOpDay(param) { 
-        this.sOpDays.set(this.arrIndex, param);
-        console.log("setSearchOpDay ", this.sOpDays);
+        this.search_opDays.set(this.arrIndex, param);
+        console.log("setSearchOpDay ", this.search_opDays);
      }
     getSearchOpDay(param) { 
-        console.log("getSearchOpDay ", this.sOpDays[param]);
-        return this.sOpDays.get(param);
+        console.log("getSearchOpDay ", this.search_opDays[param]);
+        return this.search_opDays.get(param);
     }
 
     setSearchPrice(param) {
@@ -96,16 +215,12 @@ export class Backend {
         console.log("setSearchOpDay ", this.sPrices);
     }
     getSearchPrice(param) { 
-        var buf = this.sPrices.get(param);
-        var op = this.sOpDays.get(param);
+        buf = this.sPrices.get(param)
+        op = this.search_opDays.get(param);
         var i = 0;
-        var day = new Date(this.date.timestamp).getUTCDay();
-        if(day == 0) { day = 7 } 
         for(;i<op.length;i++) {
-            if(day == op[i]) { break; }
+            if(this.day == op[i]) { break; }
         }
-        console.log(this.sPrices);
-        console.log("Buf: ", buf);
         return buf[i];
     }
 
